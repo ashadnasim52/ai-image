ff65a35f67705b24f9f445bd0d4fc4fc3fb8e731
ashad <ashadnasim72@gmail.com>
2024-02-29 07:29:55 +0530

    initail commit

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { SignIn } from '@clerk/nextjs';
+const Home = () => {
+	return <SignIn />;
+};
+
+export default Home;
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SignUp } from '@clerk/nextjs';
+
+const Home = () => {
+	return <SignUp />;
+};
+
+export default Home;
